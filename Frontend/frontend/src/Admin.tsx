789fb7b132ac1d91d41e0d4c9d32667259c1fab6
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2024-06-02 20:50:53 +0545

    new files

--- Frontend/frontend/src/Admin.tsx
+++ Frontend/frontend/src/Admin.tsx
@@ -1,83 +1,88 @@
 function App() {
     return (
         <>
-            <html>
+            <html lang="en">
             <head>
-                <title>Learning Management System</title>
-                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-                        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"></link>
-                            <link rel="stylesheet" href="css/general.css"></link>
-                                <link rel="stylesheet" href="css/header.css"></link>
-                                    <link rel="stylesheet" href="css/sidebar.css"></link>
+                <meta charSet="UTF-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+                <title>Schoolify:Admin</title>
+
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+                      rel="stylesheet"/>
+
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+                    rel="stylesheet"/>
+
+                <link rel="stylesheet" href="styles/sidebar.css"/>
+                <link rel="stylesheet" href="styles/header.css"/>
             </head>
             <body>
-            <header class="header">
-                <div class="left-area">
-                    <div class="text">
-                        <img src="icons/logoo.png" alt=""></img>
+            <header className="header">
+
+
+                <div className="left-area">
+                    <div className="header_obj">
+                        <p>DashBoard</p>
+                    </div>
+                    <div className="header_obj">
+                        <p>Notices</p>
+                    </div>
+                    <div className="header_obj">
+                        <p>Request</p>
                     </div>
                 </div>
 
-                <div class="middle-area">
-                    <div class="admin-label">
-                        <p>ADMINISTRATOR</p>
+                <div className="right-area">
+                    <div className="notif-icon">
+                        <img src="icons/notif-bell-icon-white.svg"/>
                     </div>
 
-                    <div class="academic-year">
-                        <p>Academic Year 2024-2025</p>
-                    </div>
-                </div>
-
-                <div class="right-area">
-                    <div class="notif-icon">
-                        <img src="icons/notif-bell-icon.svg" alt=""></img>
-                    </div>
-
-                    <div class="profile-icon">
-                        <img src="icons/profile-circle-icon.svg"></img>
+                    <div className="profile-icon">
+                        <img className="profile" src="icons/profile-circle-icon-white.svg"/>
+                        <p>Abhinav Dhakal</p>
+                        <img className="arrow-icon" src="icons/arrow.svg" alt=""/>
                     </div>
                 </div>
-
-
             </header>
 
-            <section class="sidebar">
-                <div class="sidebar_obj">
-                    <img src="icons/dashboard-svgrepo-com.svg" alt=""></img>
-                        <p>DashBoard</p>
+            <section className="sidebar">
+                <div className="logo">
+                    <img src="images/logoo.png" alt="Schoolify"/>
                 </div>
-                <div class="sidebar_obj">
-                    <img src="icons/add-square-svgrepo-com-2.svg" alt=""></img>
+                <div className="contents">
+                    <div className="sidebar_obj">
+                        <img src="icons/add-square-svgrepo-com-2.svg" alt=""/>
                         <p>Add Student</p>
-                </div>
-                <div class="sidebar_obj">
-                    <img src="icons/add-square-svgrepo-com-2.svg" alt=""></img>
+                    </div>
+                    <div className="sidebar_obj">
+                        <img src="icons/add-square-svgrepo-com-2.svg" alt=""/>
                         <p>Add Teacher</p>
-                </div>
-                <div class="sidebar_obj">
-                    <img src="icons/invoice-ledger-line-svgrepo-com.svg" alt=""></img>
+                    </div>
+                    <div className="sidebar_obj">
+                        <img src="icons/invoice-ledger-line-svgrepo-com.svg" alt=""/>
                         <p>Ledger</p>
+                    </div>
+                    <div className="sidebar_obj">
+                        <img src="icons/add-square-svgrepo-com-2.svg"/>
+                        <p>Teacher Attendance</p>
+                    </div>
+
+                    <div className="sidebar_obj">
+                        <img src="icons/credit-card-payment-svgrepo-com.svg" alt=""/>
+                        <p>Handle Fees</p>
+                    </div>
                 </div>
-                <div class="sidebar_obj">
-                    <p>Teacher Attendance</p>
-                </div>
-                <div class="sidebar_obj">
-                    <img src="icons/request-new-svgrepo-com.svg" alt=""></img>
-                        <p>Request</p>
-                </div>
-                <div class="sidebar_obj">
-                    <img src="icons/notice-push-svgrepo-com.svg" alt=""></img>
-                        <p>Add Notice</p>
-                </div>
-                <div class="sidebar_obj">
-                    <img src="icons/credit-card-payment-svgrepo-com.svg" alt=""></img>
-                        <p>Fee History</p>
-                </div>
+
             </section>
             </body>
             </html>
-    </>
+        </>
     )
 }
 
