fea442b1e45d79f9904aa11fddb8575b0137004c
Abhinav Dhakal <128499381+abhidhakal@users.noreply.github.com>
2024-06-02 22:13:45 +0545

    moved files to react frontend

--- ReactLmsFrontend/src/App.tsx
+++ ReactLmsFrontend/src/App.tsx
@@ -1,26 +1,8 @@
-
-
 import './App.css'
-import Header from "./components/Header.tsx";
-import Footer from "./components/Footer.tsx";
-import HomePage from "./components/HomePage.tsx";
+import Admin from "./Admin.tsx";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-          <Header />
-          <HomePage />
-          <Footer />
-
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return <div><Admin></Admin></div>
 }
 
 export default App
