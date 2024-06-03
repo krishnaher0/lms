118e95ae966d711a338300206d1366b28f95c8a6
krishnaher0 <krishna613460@gmail.com>
2024-06-03 21:48:32 +0545

    frontend development

--- ReactLmsFrontend/src/App.tsx
+++ ReactLmsFrontend/src/App.tsx
@@ -1,8 +1,40 @@
 import './App.css'
-import Teacher from "./Teacher.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import Home from "./components/Home.tsx";
+import Header from "./components/Header.tsx";
+import Admin from "./components/Admin.tsx";
+import Sidebar from "./components/Sidebar.tsx";
+import Attendance from "./components/Attendance.tsx";
+
+
+const queryClient = new QueryClient()
+
 
 function App() {
-    return <div><Teacher></Teacher></div>
+
+    return (
+        <>
+            <QueryClientProvider client={queryClient}>
+                <Header/>
+                <Sidebar/>
+
+
+                <RouterProvider router={
+                    createBrowserRouter(
+                        [
+
+                            {path: "/home", element: <Home/>},
+                            {path: "/admin", element: <Admin/>},
+                            {path: "/students", element: <Attendance/>}
+
+
+                        ]
+                    )
+                }/>
+            </QueryClientProvider>
+        </>
+    )
 }
 
 export default App
---
+++ ReactLmsFrontend/src/components/Sidebar.tsx
@@ -0,0 +1,18 @@
+
+const Sidebar=()=>{
+
+
+    return(
+
+        <>
+            <div >
+        <button>Students List</button>
+            <button>Teacher List</button>
+            <button>Class List</button>
+            <button>Attendance List</button>
+
+            </div>
+            </>
+    )
+}
+export default Sidebar
